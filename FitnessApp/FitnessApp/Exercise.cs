04d05f95b3e7c94e2a7282b8031eddd233dbc59c
Stilkatagpt <stilianh6@gmail.com>
2025-05-03 15:23:30 +0300

    Exercise.cs

--- FitnessApp/FitnessApp/Exercise.cs
+++ FitnessApp/FitnessApp/Exercise.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +10,34 @@ namespace FitnessApp
     //    Изчисляване на тренировъчен обем(sets × reps × weight)
     internal class Exercise
     {
+  
+            // Exersice name (example "Squat", "Lunge")
+            public string Name { get; set; }
+
+            public int Sets { get; set; }
+
+            public int Reps { get; set; }
+
+            public double Weight { get; set; }
+
+            public Exercise(string name, int sets, int reps, double weight)
+            {
+                Name = name;
+                Sets = sets;
+                Reps = reps;
+                Weight = weight;
+            }
+
+            public double CalculateTrainingVolume()
+            {
+                return Sets * Reps * Weight;
+            }
+
+            public void ShowInfo()
+            {
+                Console.WriteLine($"Exercise: {Name}");
+                Console.WriteLine($"Sets: {Sets}, Reps: {Reps}, Weight: {Weight} kg");
+                Console.WriteLine($"Training Volume: {CalculateTrainingVolume()} kg");
+            }
+        }
     }
-}
